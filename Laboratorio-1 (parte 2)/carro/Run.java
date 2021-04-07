a1c705212f12b855b9771991b270616aecef5c42
Marco Alvarado <marcotrabajos2013@gmail.com>
2021-04-07 17:41:49 -0600

    Se agrega la moto DUCATI

--- Laboratorio-1 (parte 2)/carro/Run.java
+++ Laboratorio-1 (parte 2)/carro/Run.java
@@ -1,6 +1,7 @@
 package carro;
 
 import Maquinaria.IMaquinaria;
+import moto.Ducati;
 import moto.Moto;
 import moto.Ninja;
 
@@ -9,7 +10,8 @@ public class Run {
 	public static void main(String[] args) {
 		new Carretera(createCarFactory(CarType.SEDAN)).runMaquinaria(); 
 		new Carretera(createCarFactory(CarType.PICKUP)).runMaquinaria();
-		new Carretera(createMotoFactory()).runMaquinaria();
+		new Carretera(createMotoFactory(MotoType.NINJA)).runMaquinaria();
+		new Carretera(createMotoFactory(MotoType.DUCATI)).runMaquinaria();
 		//agregar ducat
 	}
 	
@@ -17,6 +19,10 @@ public class Run {
 	private enum CarType {
 		SEDAN,
 		PICKUP
+	}
+	private enum MotoType {
+		NINJA,
+		DUCATI
 	}
 	
 	private static Carro createCarFactory(CarType type) {
@@ -26,8 +32,11 @@ public class Run {
 	  return new Pickup(); 
 	}
 	
-	private static Moto createMotoFactory() {
-	   return new Ninja(); 
+	private static Moto createMotoFactory(MotoType type) {
+		  if (type.equals(MotoType.NINJA)) {
+			  return new Ninja();
+		  }
+	   return new Ducati(); 
 	}
 	
 }
